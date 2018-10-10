bd3887ee95f4b0457c42a78d2395757657416824
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2018-10-10 22:38:45 +0530

    Modified .bashrc and fedora script

--- scripts/fedora-post-install.sh
+++ scripts/fedora-post-install.sh
@@ -26,7 +26,7 @@ flatpak install flathub com.github.babluboy.bookworm
 sudo dnf -y dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install vlc
 ## Steam
-dnf install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf -y install steam
 ## Upgrade plugin
 sudo dnf -y install dnf-plugin-system-upgrade
