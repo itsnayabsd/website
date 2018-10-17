891df197b7b1a4a9e6c3b1094949454fe033bb8d
Nayab Basha Sayed <nayabbashasyed@gmail.com>
2018-10-17 10:21:08 +0530

    Modified elementary_post_install.sh

--- scripts/elementary_post_install.sh
+++ scripts/elementary_post_install.sh
@@ -32,7 +32,7 @@ sudo apt -y install vim git google-chrome-stable software-properties-common \
 ruby-full openssh-server build-essential device-tree-compiler libz-dev \
 autoconf flex byacc bison sharutils libtool libssl-dev libncurses5-dev \
 libncursesw5-dev cmake minicom virtualbox cscope exuberant-ctags \
-gnome-terminal woeusb flatpak
+gnome-terminal woeusb flatpak tftpd-hpa
 # wps-office
 wget -c http://kdl1.cache.wps.com/ksodl/download/linux/a21//wps-office_10.1.0.5707~a21_amd64.deb
 wget -c http://kdl.cc.ksosoft.com/wps-community/download/fonts/wps-office-fonts_1.0_all.deb
