c9887402788cad2ea7d14098ed6ac59bec90c099
Nayab Basha Sayed <basha@nayab.xyz>
2020-04-04 00:23:59 +0530

    Ubuntu post install updated

--- scripts/ubuntu_pos_install.sh
+++ scripts/ubuntu_pos_install.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt -y dist-upgrade
 
-sudo apt install git gitk git-email cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server
+sudo apt install git gitk git-email cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server build-essential autoconf texinfo help2man gawk libtool-bin libncurses5-dev
 
 git config --global user.name "Nayab Basha Sayed"
 git config --global user.email "basha@nayab.xyz"
