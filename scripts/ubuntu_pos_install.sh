f196031c8102aa1b080f6d7129e7d5820fbffb30
Nayab Basha Sayed <basha@nayab.xyz>
2020-04-05 01:07:49 +0530

    Added device tree compiler
    Signed-off-by: Nayab Basha Sayed <basha@nayab.xyz>

--- scripts/ubuntu_pos_install.sh
+++ scripts/ubuntu_pos_install.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt -y dist-upgrade
 
-sudo apt install git gitk git-email cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server build-essential autoconf texinfo help2man gawk libtool-bin libncurses5-dev
+sudo apt install git gitk git-email cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server build-essential autoconf texinfo help2man gawk libtool-bin libncurses5-dev device-tree-compiler
 
 git config --global user.name "Nayab Basha Sayed"
 git config --global user.email "basha@nayab.xyz"
