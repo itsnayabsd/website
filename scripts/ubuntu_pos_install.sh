9bf0622ce9f317689e1b31cceff3d8f439f20c5b
Nayab Basha Sayed <basha@nayab.xyz>
2020-04-27 00:20:14 +0530

    Added few packages to ubuntu install script
    Signed-off-by: Nayab Basha Sayed <basha@nayab.xyz>

--- scripts/ubuntu_pos_install.sh
+++ scripts/ubuntu_pos_install.sh
@@ -4,7 +4,7 @@
 sudo apt update
 sudo apt -y dist-upgrade
 
-sudo apt install -y vim gnome-shell-extensions gnome-tweaks git gitk git-email ruby-full cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server build-essential autoconf texinfo help2man gawk libtool-bin libncurses5-dev device-tree-compiler zlib1g-dev locate python python3-distutils python3-dev ccache gettext libapr1 libaprutil1 libcroco3 libserf-1-1 libsvn1 libutf8proc2 subversion xsltproc tree geary flatpak libelf-dev ecj fastjar java-propose-classpath libncursesw5-dev libgconf2-dev libcanberra-gtk-module libcanberra-gtk3-module net-tools
+sudo apt install -y vim gnome-shell-extensions gnome-tweaks git gitk git-email ruby-full cscope libssl-dev bison flex gcc-arm-linux-gnueabi picocom gcc-arm-linux-gnueabi nfs-kernel-server build-essential autoconf texinfo help2man gawk libtool-bin libncurses5-dev device-tree-compiler zlib1g-dev locate python python3-distutils python3-dev ccache gettext libapr1 libaprutil1 libcroco3 libserf-1-1 libsvn1 libutf8proc2 subversion xsltproc tree geary flatpak libelf-dev ecj fastjar java-propose-classpath libncursesw5-dev libgconf2-dev libcanberra-gtk-module libcanberra-gtk3-module net-tools openvpn dialog python3-pip python3-setuptools
 
 git config --global user.name "Nayab Basha Sayed"
 git config --global user.email "basha@nayab.xyz"
@@ -18,6 +18,8 @@ echo "alias xo='xdg-open '" >> ~/.bashrc
 echo "alias gitc='git commit -s '" >> ~/.bashrc
 
 gem install jekyll bundler
+
+sudo pip3 install protonvpn-cli
 
 echo "sudo updatedb might take a while"
 sudo updatedb
