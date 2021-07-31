bbb4973e0051212e86e4f39edbb15811bd361e18
Nayab Syed <nayabbashasyed@gmail.com>
2021-07-31 23:51:15 +0530

    rust7

--- scripts/kubuntu_setup.sh
+++ scripts/kubuntu_setup.sh
@@ -46,7 +46,7 @@ android_tools() {
 
 install_dev() {
 	sudo apt update
-	sudo apt install -y cscope universal-ctags build-essential libssl-dev libreadline-dev zlib1g-dev network-manager-openvpn openssh-server youtube-dl libgconf-2-4 ktorrent gimp easytag dos2unix minicom wireshark
+	sudo apt install -y vim cscope universal-ctags build-essential libssl-dev libreadline-dev zlib1g-dev network-manager-openvpn openssh-server youtube-dl libgconf-2-4 ktorrent gimp easytag dos2unix minicom wireshark
 }
 
 cascadia_font() {
@@ -60,7 +60,7 @@ dist_upgrade() {
 }
 
 install_brave() {
-	sudo apt install -y vim apt-transport-https curl
+	sudo apt install -y apt-transport-https curl
 	sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 	echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 	sudo apt update
